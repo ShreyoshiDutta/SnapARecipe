8b06c8e2e7b000b8819d9fc8024b60533b3001e3
Sree <msruna@gmail.com>
2017-04-11 19:42:31 +1000

    ADDS RECIPE LIST VIEW

---
+++ app/src/main/java/com/sree/snaparecipe/ListRecipes.java
@@ -0,0 +1,13 @@
+package com.sree.snaparecipe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListRecipes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_recipes);
+    }
+}
